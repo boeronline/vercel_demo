750a465057e0a26084cb5c7680102ad018893868
Steven <96180461+steven-deboer@users.noreply.github.com>
2025-10-08 16:54:54 +0200

    Handle HEAD requests in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  if (request.method === 'HEAD') {
+    return NextResponse.next();
+  }
+
   const { pathname } = request.nextUrl;
 
   if (pathname === '/' || pathname.startsWith('/_next') || pathname.startsWith('/icon') || pathname === '/favicon.ico') {
